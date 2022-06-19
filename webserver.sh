7fb1f4c94860903c69e5b33df2d36a7ac2111f02
NLshadow <55489716+NLshadow@users.noreply.github.com>
2022-06-20 01:11:46 +0200

    Update webserver.sh

--- webserver.sh
+++ webserver.sh
@@ -32,11 +32,16 @@ echo "Creating self-signed cert"
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/webserver.key -out /etc/ssl/certs/webserver.crt
 
 wget https://raw.githubusercontent.com/NLshadow/School/main/wordpress.conf -P /etc/apache2/sites-available/
+sleep 5&
 
 echo "Change redirect address to correct dns name!"
 vim /etc/apache2/sites-available/wrodpress.conf
 sleep 5&
 
+sudo a2enmod ssl
+sleep 5&
+sudo service apache2 restart
+sleep 10&
 sudo a2ensite wordpress.conf
 sleep 10&
 sudo service apache2 restart
