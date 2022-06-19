55a50984f4be2ae4ddd3230863e5cd0727c902f9
NLshadow <55489716+NLshadow@users.noreply.github.com>
2022-06-20 00:29:24 +0200

    Update webserver.sh

--- webserver.sh
+++ webserver.sh
@@ -33,7 +33,9 @@ sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/privat
 wget https://raw.githubusercontent.com/NLshadow/School/main/wordpress.conf -P /etc/apache2/sites-available/
 
 echo "Change redirect address to correct dns name!"
+vim /etc/apache2/sites-available/wrodpress.conf
 sleep 5&
 
 sudo a2ensite wordpress.conf
+sleep 10&
 sudo service apache2 restart
