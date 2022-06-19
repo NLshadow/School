1bd2590fe571ddf57675d112c777dc890b55c81b
NLshadow <55489716+NLshadow@users.noreply.github.com>
2022-06-20 00:25:39 +0200

    Create certs.sh

---
+++ certs.sh
@@ -0,0 +1,10 @@
+echo "Creating self-signed cert"
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/webserver.key -out /etc/ssl/certs/webserver.crt
+
+wget https://raw.githubusercontent.com/NLshadow/School/main/wordpress.conf -P /etc/apache2/sites-available/
+
+echo "Change redirect address to correct dns name!"
+sleep 5&
+
+sudo a2ensite wordpress.conf
+sudo service apache2 restart
