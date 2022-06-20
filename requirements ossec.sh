cc1eaa913f6153d949d240da7931d91d376c1c8e
NLshadow <55489716+NLshadow@users.noreply.github.com>
2022-06-20 22:04:24 +0200

    Update requirements ossec.sh

--- requirements ossec.sh
+++ requirements ossec.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo apt-get install build-essential make zlib1g-dev libpcre2-dev libevent-dev libssl-dev libsqlite3-dev
+sudo apt-get install build-essential make zlib1g-dev libpcre2-dev libevent-dev libssl-dev libsqlite3-dev -y
 
 wget -q -O - https://updates.atomicorp.com/installers/atomic | sudo bash
-apt-get update
-apt-get install ossec-hids-agent
+apt-get update -y
+apt-get install ossec-hids-agent -y
